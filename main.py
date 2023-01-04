0d19ad8ee52599c8392432a57fd9bcd4ea7b48dd
Ben O'Connell <boconne4@gmail.com>
2023-01-04 12:40:58 -0600

    Created get_games() for Players

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ from players import Players
 players = Players((['Byran Reynolds']), ['Pirates'])
 print(players.players)
 print(players.teams)
+print(players.get_plays())
--- players.py
+++ players.py
@@ -4,12 +4,17 @@ import statsapi
 class Players: 
     def __init__(self,players,player_teams):
         self.players = players
-        self.teams = self.get_team_ids2(player_teams)
+        self.teams = self.get_team_ids(player_teams)
 
-    # def get_plays ():
-    #     pass 
+    def get_plays (self):
+        games = []
+        for team in self.teams:
+            new_games = statsapi.schedule(start_date="07/01/2018", end_date="07/31/2018", team=team)
+            for g in new_games:
+                games.append(g)
+        return games
 
-    def get_team_ids2(self,player_teams):
+    def get_team_ids(self,player_teams):
         results = []
         all_teams = statsapi.get('teams', {'leagueIds':104})
         for team in player_teams:
