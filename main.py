7b42feec18ecee96fbca26174bd7101312a1cbd9
Ben O'Connell <boconne4@gmail.com>
2023-01-04 12:56:50 -0600

    Created get_plays() gets all scoring plays players are involved in

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from players import Players
 
 
-players = Players((['Byran Reynolds']), ['Pirates'])
+players = Players((['Bryan Reynolds']), ['Pirates','Dodgers','Brewers','Yankees'])
 print(players.players)
 print(players.teams)
+print(players.games)
 print(players.get_plays())
--- players.py
+++ players.py
@@ -5,14 +5,16 @@ class Players:
     def __init__(self,players,player_teams):
         self.players = players
         self.teams = self.get_team_ids(player_teams)
+        self.games = self.get_games()
 
-    def get_plays (self):
+    def get_games (self):
         games = []
         for team in self.teams:
-            new_games = statsapi.schedule(start_date="07/01/2018", end_date="07/31/2018", team=team)
+            new_games = statsapi.schedule(start_date="07/01/2022", end_date="07/31/2022", team=team)
             for g in new_games:
-                games.append(g)
+                games.append(g['game_id'])
         return games
+        
 
     def get_team_ids(self,player_teams):
         results = []
@@ -21,4 +23,16 @@ class Players:
             for ateam in all_teams['teams']:
                 if team in ateam['name']:
                     results.append(ateam['id'])
+        return results
+
+    def get_plays(self):
+        results = []
+        game_plays = []
+        for game in self.games:
+            game_plays.append(statsapi.game_scoring_play_data(game))
+        for game in game_plays:
+            for play in game['plays']:
+                for player in self.players:
+                    if player in play['result']['description']:
+                        results.append(play)
         return results
