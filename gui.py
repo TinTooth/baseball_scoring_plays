907d252c323ec81991c4a96eb29b47294fffa0bd
Ben O'Connell <boconne4@gmail.com>
2023-01-04 14:35:08 -0600

    Added Refresh List Functionality

--- gui.py
+++ gui.py
@@ -1,18 +1,25 @@
+import textwrap
 from plays import Plays
 import PySimpleGUI as sg
 
 
-plays = Plays((['Bryan Reynolds']), ['Pirates'])
+plays = Plays(['Bryan Reynolds','Aaron Judge'], ['Pirates'])
 scoring_plays = plays.get_plays()
+
+button_row = [[sg.Button(player) for player in plays.players]]
+
 layout = [
-    [sg.Text(plays.players)],
-    [sg.Listbox(values =scoring_plays, size=(50,50), key = '-Plays-')],
+    [button_row],
+    [sg.Listbox(values =scoring_plays, size=(50,None), key = 'Plays')],
     [sg.Button('Refresh')]]
 window = sg.Window("Scoring Plays",layout,margins=(100,100))
 while True: 
     event,values = window.read()
     if event == sg.WIN_CLOSED:
         break
+    if event == 'Refresh':
+        window['Plays'].update(plays.get_plays())
+
 # sg.Window(title="Scoring Plays",layout = [[sg.Text('Scoring Plays')],[sg.Button('Refresh')]], margins =(200,100)).read()
 
 window.close()
